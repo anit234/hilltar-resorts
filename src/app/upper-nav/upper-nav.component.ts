356ea6e9a29d6d054130234e9ab2b2f5e1ae3f98
anit123 <anitjee@gmail.com>
2024-01-15 19:21:41 -0700

    first commit

---
+++ src/app/upper-nav/upper-nav.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upper-nav',
+  templateUrl: './upper-nav.component.html',
+  styleUrls: ['./upper-nav.component.scss']
+})
+export class UpperNavComponent {
+
+}
